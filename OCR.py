c5ba4e464b252a55450c11fecbe89e8f400bb3a4
cham_why <53082717+chamwhy@users.noreply.github.com>
2021-09-09 16:19:15 +0900

    Update OCR.py

--- OCR.py
+++ OCR.py
@@ -3,9 +3,9 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 img = cv2.imread("img/sample.png", cv2.IMREAD_COLOR)
+img1 = img.copy()
 
-
-grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+grayImg = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 
 ret, binaryImg = cv2.threshold(grayImg, 127, 255, cv2.THRESH_BINARY_INV)
 
@@ -13,6 +13,14 @@ kernel = np.ones((7, 7), np.uint8)
 
 dilationImg = cv2.dilate(binaryImg, kernel, iterations=1)
 
-cv2.imshow('sample', dilationImg)
+contours, hierachy = cv2.findContours(dilationImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+drawedImf = cv2.drawContours(binaryImg, contours, -1, (255, 255, 0), 2)
+xticks = []
+for cnt in contours:
+    x, y, w, h = cv2.boundingRect(cnt)
+    xticks.append()
+    img1 = cv2.rectangle(img1,(x,y),(x+w, y+h),(0,255,0), 3)
+
+cv2.imshow('sample', img1)
 cv2.waitKey(0)
 
